e546037c90657047184a6d8a15288a6663ef7824
Hrishikesh Nikam <hrishinikam2000@gmail.com>
2021-04-25 17:07:46 +0530

    Add Message box, removed LastName box

--- GUI_ML.py
+++ GUI_ML.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import *
 import csv
 import os
 from tkinter import ttk
 
 gui = Tk()
+gui.geometry("300x300")
 
 gui.title('Machine Learning GUI')
 
@@ -14,27 +16,21 @@ class GUI(Frame):
         Frame.__init__(self, master)
         self.grid()
 
-        self.fnameLabel = Label(master, text="First Name")
+        self.fnameLabel = Label(master, text="Enter Message as Recieved")
         self.fnameLabel.grid()
 
         self.fnameEntry = Entry(master)
         self.fnameEntry.grid()
 
-        self.lnameLabel = Label(master, text="Last Name")
-        self.lnameLabel.grid()
 
-        self.lnameEntry = Entry(master)
-        self.lnameEntry.grid()
 
         self.submitButton = Button(master, command=self.buttonClick, text="Submit")
         self.submitButton.grid()
 
 
-    def buttonClick(self):
-    """ handle button click event and output text from entry area"""
-    print('hello')    # Trigger action tommorow
-
-
+    def buttonClick(self,master=None):
+        print('hello')
+        
 
 if __name__ == "__main__":
     guiFrame = GUI()
